682e258784212fc4dddced3bc98c951c370e8533
MYLO175 <92120440+MYLO175@users.noreply.github.com>
2025-09-22 20:36:29 +0100

    added waitlist to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import WaitlistEntry
 
-# Register your models here.
+@admin.register(WaitlistEntry)
+class WaitlistEntryAdmin(admin.ModelAdmin):
+    list_display = ("email", "name", "created_at")
+    search_fields = ("email", "name")
