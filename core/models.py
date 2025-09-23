fbd0426feea17130e9b334a5ce3f2a260fef2cf9
MYLO175 <92120440+MYLO175@users.noreply.github.com>
2025-09-23 17:18:58 +0100

    Add phone to waitlist

--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class WaitlistEntry(models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=200, blank=True)
-    company = models.CharField(max_length=200, blank=True)
+    country_code = models.CharField(max_length=8, blank=True)   # e.g. +44
+    phone_number = models.CharField(max_length=32, blank=True)  # e.g. 7442027268
     note = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
